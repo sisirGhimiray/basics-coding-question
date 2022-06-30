d2d5f9027ae11102d286889182c7ae5d580a3f88
sisirGhimiray <ghimiraysisir150@gmail.com>
2022-06-30 21:52:34 +0530

    question 14 sub 2 ques solved

--- 14.c
+++ 14.c
@@ -30,6 +30,7 @@ return -1;
 int p_find_indexF(char *str1,char *str2){
 char *p1=NULL;
 char *p2=str1;
+
 while(*str1!='\0'){
 if(*str1==*str2&&(*(str1-1)==32)){
   p1=str1;
@@ -37,22 +38,55 @@ if(*str1==*str2&&(*(str1-1)==32)){
     str1++;
     str2++;
   }
-  if(*str2=='\0'){
+  if(*str1=='\0'){
   return p1-p2;
   }
 }
 
 str1++;
 }
+printf("hello\n");
+return -1;
+}
+
+
+
+int p_find_IndexL(char *str1,char *str2){
+int canStart=0;
+char *s2=str2;
+char *s1=str1;
+char *p1=NULL;
+while(*str1!='\0'){
+
+if(*str1==*str2&&(*(str1-1))==32){
+  p1=str1;
+canStart=1;
+
+}else{
+  canStart=0;
+}
+while(canStart&&*str2!='\0'){
+  str1++;
+  str2++;
+}
+if(*str2=='\0'){
+str2=s2;
+}
+
+  str1++;
+}
+if(*str1=='\0'){
+  return p1-s1;
+}
+
+
 return -1;
 }
 
 
 int main(void){
-
-char str[50]="what is this this is an apple";
-char str2[]="is";
-printf("%d",p_find_indexF(str,str2));
-
+char s[100]="this that what that th  want that right that ok that";
+char t[20]="that";
+printf("%d\n",p_find_IndexL(s,t));
   return 0;
 }
